1929dc3ec0a17668b9c235c2372d08b4e6058c96
AshishWadhai13 <ashishwadhai2@gmail.com>
2025-04-20 00:16:29 +0530

    Delete E-mart directory

--- E-mart/src/main/java/com/example/service/OrderDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.entity.OrderDetails;
-
-import java.util.List;
-
-public interface OrderDetailsService {
-    List<OrderDetails> getOrderDetailsByOrderId(int orderId);
-}
